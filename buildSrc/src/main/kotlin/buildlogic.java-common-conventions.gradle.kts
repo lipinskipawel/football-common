a314a9918ef9f99c179bd1cb7cccadccbeee1972
Pawel Lipinski <lipinskijob@gmail.com>
2025-06-19 08:04:24 +0200

    Add gradle init project

---
+++ buildSrc/src/main/kotlin/buildlogic.java-common-conventions.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    constraints {
+        implementation("org.apache.commons:commons-text:1.11.0")
+    }
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
